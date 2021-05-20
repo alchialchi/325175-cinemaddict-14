8b699f3491c84513600829fd29540f88cf2b382e
Olga Lukina <obeskurajen@mail.ru>
2021-05-20 16:50:44 +0200

    Update const

--- src/const.js
+++ src/const.js
@@ -5,9 +5,49 @@ export const SortType = {
   RATING: 'rate',
 };
 
-export const FilterType = {
-  ALL: 'All',
-  WATCHLIST: 'Watchlist',
+export const MenuItem = {
+  ALL_MOVIES: 'All movies',
+  WATHCLIST: 'Watchlist',
+  FAVOURITES: 'Favorites',
   HISTORY: 'History',
-  FAVORITES: 'Favorites',
+  STATISTICS: 'Statistics',
+};
+
+export const UserAction = {
+  UPDATE_MOVIE: 'Update movie',
+  UPDATE_COMMENTS: 'Update comments',
+  ADD_COMMENT: 'Add comment',
+  DELETE_COMMENT: 'Delete comment',
+};
+
+export const UpdateType = {
+  PATCH: 'Patch',
+  MINOR: 'Minor',
+  MAJOR: 'Major',
+  INIT: 'Init',
+};
+
+export const Rank = {
+  NOVICE: 'Novice',
+  FAN: 'Fan',
+  MOVIE_BUFF: 'Movie Buff',
+};
+
+export const Mode = {
+  DEFAULT: 'Default',
+  POPUP: 'Popup',
+};
+
+export const TimeRange = {
+  ALL_TIME: 'all-time',
+  DAY: 'day',
+  WEEK: 'week',
+  MONTH: 'month',
+  YEAR: 'year',
+};
+
+export const State = {
+  SAVING: 'Saving',
+  DELETING: 'Deleting',
+  ABORTING: 'Aborting',
 };
